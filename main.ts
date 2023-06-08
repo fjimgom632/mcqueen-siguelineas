111e3d8ccb82dc913e8d112d8bd442c6f523e207
fjimgom632 <99208736+fjimgom632@users.noreply.github.com>
2023-06-08 09:14:44 +0200

    Sigue líneas con luces RGB

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 let velocidad = 50
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    basic.pause(1000)
+    strip.showRainbow(randint(0, 4), neopixel.hsl(randint(0, 150), randint(0, 150), randint(0, 150)))
+    strip.show()
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
     } else {
